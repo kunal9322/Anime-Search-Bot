4499434d4da77dd3d2713a562bacf3c819741a59
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-04-10 15:12:55 +0700

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -2,20 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from config import DB_URI
+from config import DATABASE_URL
 
 count_ = 0
 def start() -> scoped_session:
-    if DB_URI == "":
+    if DATABASE_URL == "":
         if count_ < 1:
             count += 1
             return print("Database url not provided..\nBut this time I won't stop 😉")
         return
-    engine = create_engine(DB_URI)
+    engine = create_engine(DATABASE_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-if DB_URI != "":
+if DATABASE_URL != "":
     BASE = declarative_base()
     SESSION = start()
