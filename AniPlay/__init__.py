0506fdb50131379d5c991556ada263f02182d63d
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-05-07 13:08:12 +0530

    Update __init__.py

--- AniPlay/__init__.py
+++ AniPlay/__init__.py
@@ -5,5 +5,5 @@ app = Client(
     "AniPlay",
     api_id= int(os.environ.get("APP_ID", "16743442")),
     api_hash= os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a"),
-    bot_token= os.environ.get("TOKEN", "5923413028:AAHe3jE8FCOQD8Cz-kP_LTn6bm2tzSyj_hE")
+    bot_token= os.environ.get("TOKEN", "5923413028:AAEFyyapzMV0OhSvfgp5ajU3DRNoonlYSyU")
 )
